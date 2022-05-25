6572104c59d957b74447a8fe2a9acf70d077ad1e
yakiv-k <yakiv.y.k@gmail.com>
2022-05-25 16:36:18 -0400

    first commit: minor changes

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import "./Header.scss";
+
+function Header() {
+    return (
+        <header>
+          <nav>
+            <img src="assets/Logo/BrainFlix-logo.svg"/>
+            <input></input>
+            <img/>
+            <button>UPLOAD</button>
+            <img/>
+          </nav>
+        </header>
+      );
+}
+
+export default Header;
