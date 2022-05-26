8cba6874bebb6a1a194f5bd42a010e3acec7dbc4
yakiv-k <yakiv.y.k@gmail.com>
2022-05-25 22:53:39 -0400

    Mobile header complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,27 @@
 import "./Header.scss";
+import Logo from "../../assets/Logo/BrainFlix-logo.svg";
+import Avatar from "../../assets/Images/Mohan-muruge.jpg";
+// import testArray from "../../assets/Data/video.json";
 
 function Header() {
-    return (
-        <header>
-          <nav>
-            <img src="assets/Logo/BrainFlix-logo.svg"/>
-            <input></input>
-            <img/>
-            <button>UPLOAD</button>
-            <img/>
-          </nav>
-        </header>
-      );
+  return (
+    <header>
+      <nav className="nav"> 
+        <img className="nav__logo" src={Logo} />
+        <form className="nav__form form">
+          <div className="nav__division">
+          <input className="form__input" placeholder="Search"></input>
+          <img
+            className="form__avatar"
+            src={Avatar}
+          />            
+          </div>
+          <button className="form__button">UPLOAD</button>
+        </form>
+        <img className="nav__avatar" src={Avatar} />
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
