5584ee7591a1325a6d19be843a670876303a0128
yakiv-k <yakiv.y.k@gmail.com>
2022-06-05 23:23:50 -0400

    Completed styles for desktop Upload desktop page

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -3,28 +3,48 @@ import { Link } from "react-router-dom";
 import image from "../../assets/Images/Upload-video-preview.jpg";
 
 function Upload() {
-    return (
-<section className="upload">
-    <div className="upload__title-container">
+  return (
+    <section className="upload">
+      <div className="upload__title-container">
         <h1 className="upload__title">Upload Video</h1>
-    </div>
-    <div className="upload__thumbnail-container">
-        <p className="upload__thumbnail-title">VIDEO THUMBNAIL</p>
-        <img className="upload__thumbnail" src={image} alt="current video thumbnail"></img>
-    </div>
-    <form className="upload__form form">
-        <label className="form__label">TITLE YOUR VIDEO</label>
-        <input className="form__input" placeholder="Add a title to your video"></input>
-        <label className="form__label">ADD A VIDEO DESCRIPTION</label>
-        <input className="form__input--padding" placeholder="Add a description to your video"></input>
-        <div className="form__button-division">
-        <button className="form__button">PUBLISH</button>
-        {/* VERIFY THE HREF PATH */}
-        <Link to="/" className="form__button--cancel" href="../Homepage/Homepage.js">CANCEL</Link>            
+      </div>
+      <form className="upload__form form">
+        <div className="form__desktop-container">
+        <div className="upload__thumbnail-container">
+          <p className="upload__thumbnail-title">VIDEO THUMBNAIL</p>
+          <img
+            className="upload__thumbnail"
+            src={image}
+            alt="current video thumbnail"
+          ></img>
         </div>
-    </form>
-</section>    
-    )
-};
+        <div className="form__desktop-container--text">
+          <label className="form__label">TITLE YOUR VIDEO</label>
+          <input
+            className="form__input"
+            placeholder="Add a title to your video"
+          ></input>
+          <label className="form__label">ADD A VIDEO DESCRIPTION</label>
+          <input
+            className="form__input--padding"
+            placeholder="Add a description to your video"
+          ></input>
+        </div>
+        </div>
+        <div className="form__button-division">
+          <button className="form__button">PUBLISH</button>
+          {/* VERIFY THE HREF PATH */}
+          <Link
+            to="/"
+            className="form__button--cancel"
+            href="../Homepage/Homepage.js"
+          >
+            CANCEL
+          </Link>
+        </div>
+      </form>
+    </section>
+  );
+}
 
 export default Upload;
